5744ff5aeb8db1316b63f3fc90724c62740902ba
Thiago Oliveira <thiago@outlook.com.br>
2021-01-22 21:33:47 -0300

    Initial Commit

---
+++ LoginViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginViewCode
+//
+//  Created by Thiago Oliveira on 22/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
